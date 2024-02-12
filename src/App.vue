8613cf211a8225c862e958d5bc49b3f12a99dd67
xiexie <2947171285@qq.com>
2024-02-12 22:51:44 +0800

    '提交么有格式化的代码'

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const str = '谢谢'
+</script>
+
+<template>
+  <div class="box">
+    <h1>我是app</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.box {
+  width: 600px;
+  height: 400px;
+  background: black;
+}
+</style>
